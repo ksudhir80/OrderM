0451a76183e7dcb36fd3bfb7a9e3024fc130a79d
ksudhir <sudhir.kunnure@gmail.com>
2025-03-21 17:14:43 +0530

    changes

--- src/OrderM.js
+++ src/OrderM.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './AppOrderM';
+import AppOrderM from './AppOrderM';
 import { Provider } from 'react-redux';
 import store from './store';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 const OrderM=()=>{
